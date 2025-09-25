ac91f5618f890dceca7aaec4facfb575fee709f4
Prosper Percy Mawunyo Dzieketey <pmdpercy@gmai.com>
2025-09-25 13:15:37 +0000

    Add 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,17 @@
-"No argument";
-"Argument found";
-"Arguments found";
-console.log("No argument");
-console.log("Argument found");
-console.log("Arguments found");
+//"No argument";
+//"Argument found";
+//"Arguments found";
+//console.log("No argument");
+//console.log("Argument found");
+//console.log("Arguments found");
 
+
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+  console.log("No argument");
+} else if (args[1] === undefined) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
